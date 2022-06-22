a727004927358ae56fec7133524c5fad285e646d
sitozeke <sitozeke@gmail.com>
2022-06-22 04:04:41 +0100

    calculator using prompt

---
+++ index.js
@@ -0,0 +1,12 @@
+    //first make 2 variables for the data inpu
+    let x,y;
+    x = parseFloat(parseInt(prompt("enter the first number"))); 
+    y = parseFloat(parseInt(prompt("enter the second number"))) ;
+    let z = x+y;
+    let a = x-y;
+    let b = x*y;
+    let c = x/y; 
+    alert("the sum of given numbers is:" +z);
+    alert("the difference of given numbers is:" +a);
+    alert("the product of given numbers is:" +b);
+    alert("the quotient of given numbers is:" +c);
